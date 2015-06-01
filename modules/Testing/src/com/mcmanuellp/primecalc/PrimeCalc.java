41c24072971aa0d92ea3ed15b90f46fbc8337425
MCManuelLP <mcmanuellp@gmail.com>
2015-06-01 03:00:28 +0200

    additional interesting information

--- modules/Testing/src/com/mcmanuellp/primecalc/PrimeCalc.java
+++ modules/Testing/src/com/mcmanuellp/primecalc/PrimeCalc.java
@@ -9,10 +9,14 @@ public class PrimeCalc
 {
 	public static void main(String[] args) throws InterruptedException
 	{
+		int maxThreadsRunning = 0;
 		CopyOnWriteArrayList<Integer> primeNumbers = new CopyOnWriteArrayList<>();
 		//starting threads to calculate every possible divisor
+		long start = System.nanoTime();
 		for(int i = 2; i < 10000; i++)
 		{
+			int threadsRunning = Thread.activeCount();
+			if(threadsRunning > maxThreadsRunning) maxThreadsRunning = threadsRunning;
 			ThreadCalc t = new ThreadCalc(i, primeNumbers);
 			t.start();
 		}
@@ -21,7 +25,12 @@ public class PrimeCalc
 		{
 			SleepUtil.sleepM(1);
 		}
-		System.out.println("threaded: " + primeNumbers.size());
+		long end = System.nanoTime();
+		long time = end - start;
+		float timeS = (float)time / (float)(Math.pow(10, 9));
+		System.out.printf("Max amount of Threads ran at the same time: %d\n", maxThreadsRunning);
+		System.out.printf("Time used to process: %.3fs (%dns)\n", timeS, time);
+		System.out.printf("Found: %d prime numbers\n", primeNumbers.size());
 		Collections.sort(primeNumbers);
 		primeNumbers.stream().filter(i -> i > 9900).forEach(System.out::println);
 	}
